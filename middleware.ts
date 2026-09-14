6724784e4893a60786f4b4e2a1a98559fcc09926
kylebutler123-create <kylebutler123@hotmail.co.uk>
2026-09-14 21:20:18 +0100

    Show captured delivery location map inside proof form

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ const CSP=[
   "default-src 'self'",
   "base-uri 'self'",
   "form-action 'self'",
+  "frame-src 'self' https://www.openstreetmap.org",
   "frame-ancestors 'none'",
   "object-src 'none'",
   "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
