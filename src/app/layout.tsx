57c4625d9cf6ec547659f4e6f02072b728167042
kylebutler123-create <kylebutler123@hotmail.co.uk>
2026-09-25 13:42:41 +0100

    Polish desktop customer account header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import './home-hero.css';
 import './mobile-polish.css';
+import './customer-desktop-header.css';
 import './dashboard-polish.css';
 import './admin-dashboard-polish.css';
 import './admin-header-colors.css';
