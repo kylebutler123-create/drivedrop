08409f040d5eb9d3ae0f7bcfd788bcb37457a8e4
kylebutler123-create <kylebutler123@hotmail.co.uk>
2026-09-26 20:42:41 +0100

    Compact six admin management cards on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ import './admin-overview-desktop.css';
 import './admin-overview-mobile.css';
 import './admin-action-centre-mobile.css';
 import './admin-user-manager-mobile.css';
+import './admin-section-mobile.css';
 import './admin-verification-expander.css';
 import './account-edit.css';
 import './transporter-profile.css';
