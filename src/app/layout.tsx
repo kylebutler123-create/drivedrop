649ae89748602bfe5659ff4ce282f71164de53e0
kylebutler123-create <kylebutler123@hotmail.co.uk>
2026-09-26 01:02:07 +0100

    Compact desktop transporter filter and notification actions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ import './active-delivery-vehicle-details.css';
 import './customer-completed-summary.css';
 import './customer-quoted-request-highlight.css';
 import './customer-desktop-action-buttons.css';
+import './transporter-desktop-action-buttons.css';
 import Link from 'next/link';
 import {currentUser} from '@/lib/auth';
 import MessagesNavLink from '@/app/components/MessagesNavLink';
